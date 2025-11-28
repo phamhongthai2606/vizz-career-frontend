83fb58c91816c6d4b2686b05d69feda129118c15
tran-thi-giang <tran.thi.giang@vinicorp.com.vn>
2025-11-28 15:49:16 +0700

    update section component in page HOME

--- src/components/pages/home/usePageHome.ts
+++ src/components/pages/home/usePageHome.ts
@@ -1,24 +1,291 @@
+import { useState } from "react";
+
 export const usePageHome = () => {
-  // Slider Logo
-  const logos = [
-    "/home/logo-slider/image1.png",
-    "/home/logo-slider/image2.png",
-    "/home/logo-slider/image3.png",
-    "/home/logo-slider/image4.png",
-    "/home/logo-slider/image5.png",
-    "/home/logo-slider/image6.png",
-  ];
-  const getRepeatedLogos = (logos: string[], minCount = 12) => {
-    const result = [];
-    while (result.length < minCount) {
-      result.push(...logos);
-    }
-    return result;
+  // handle like job
+  const [likedIds, setLikedIds] = useState<(number | string)[]>([]);
+  const toggleLike = (id: number | string) => {
+    setLikedIds((prev) =>
+      prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id],
+    );
   };
-  const repeatedLogos = getRepeatedLogos(logos);
+
+  // call API
+  const jobsRecommend = [
+    {
+      id: 1,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "東京23区",
+      category: "教育",
+      schedule: "週1~3日以上",
+      company: "株式会社BlueAI",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job1.png",
+      position: "営業、エンジニア、デザイナー、経理、マーケティング",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 2,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job2.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 3,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job3.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 4,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job4.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 5,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job5.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 6,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job6.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 7,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 8,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 9,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 10,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 11,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 12,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+  ];
+
+  const jobs = [
+    {
+      id: 4,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "東京23区",
+      category: "教育",
+      schedule: "週1~3日以上",
+      company: "株式会社BlueAI",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job4.png",
+      position: "営業、エンジニア、デザイナー、経理、マーケティング",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 5,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job5.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 6,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job6.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 7,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+  ];
+
+  const jobsFeatured = [
+    {
+      id: 4,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "東京23区",
+      category: "教育",
+      schedule: "週1~3日以上",
+      company: "株式会社BlueAI",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job4.png",
+      position: "営業、エンジニア、デザイナー、経理、マーケティング",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 5,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job5.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 6,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "/home/job/job6.png",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+    {
+      id: 7,
+      title:
+        "【東大早慶8割｜MBB等大手内定多数】未経験から裁量大きく自分の適正と希望に合った職種に挑戦！",
+      location: "大阪市",
+      category: "IT",
+      schedule: "週5日",
+      company: "株式会社Tech",
+      companyLogo: "/home/logo-company.png",
+      image: "",
+      position: "営業職",
+      salary: "時給1,200円〜",
+    },
+  ];
 
   return {
-    // Slider Logo
-    repeatedLogos,
+    // handle like job
+    likedIds,
+    toggleLike,
+    // call API
+    jobsRecommend,
+    jobs,
+    jobsFeatured,
   };
 };
